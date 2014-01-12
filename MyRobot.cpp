527ef302a765569c2e22480a034401539733b5c2
Tyler North <tyler.north@team2342.org>
2014-01-12 16:42:35 -0500

    Added Test Mode.

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,5 +1,5 @@
 #include "WPILib.h"
-
+#include "TestMode.h"
 /**
  * This is a demo program showing the use of the RobotBase class.
  * The SimpleRobot class is the base of a robot application that will automatically call your
@@ -50,6 +50,10 @@ public:
 	 * Runs during test mode
 	 */
 	void Test() {
+		TestMode tester;
+		while (IsTest()){
+			tester.performTesting();
+		}
 
 	}
 };
