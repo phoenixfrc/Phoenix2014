10b57f12a841bfe011dba513a5390942ab5c53ca
team2342 <controls@team2342.org>
2014-01-23 20:11:55 -0500

    I made grabber code button based, and made shooter state machine.

--- Grabber.cpp
+++ Grabber.cpp
@@ -4,17 +4,32 @@
 
 
 Grabber::Grabber() :
-	grabberPuller(PHOENIX2014_GRABBER_ACTUATOR)
+	grabberPuller(PHOENIX2014_GRABBER_ACTUATOR),
+	grabberElevator(PHOENIX2014_GRABBER_ELEVATOR_PWM)
 {
 
 }
 
 void Grabber::OperateGrabber(Joystick * gamePad){
 	
-	bool openGrabber = gamePad->GetRawButton(2);
+	bool openGrabberButton = gamePad->GetRawButton(1);
+	bool closeGrabberButton = gamePad->GetRawButton(3);
+	bool moveGrabberUpButton = gamePad->GetRawButton(4);
+	bool moveGrabberDownButton = gamePad->GetRawButton(2);
 	
-	if (openGrabber){
-	grabberPuller.Set(Relay::kOn);
+	//this should open the grabber when you press the open button
+	if (openGrabberButton){
+	grabberPuller.Set(Relay::kReverse);
+	}
+	//this should close the grabber when you press the close button
+	if (closeGrabberButton){
+		grabberPuller.Set(Relay::kForward);
+	}
+	if (moveGrabberUpButton){
+		grabberElevator.Set(1.0);
+	}
+	if (moveGrabberDownButton){
+		grabberElevator.Set(-1.0);
 	}
 }
 
--- Shooter.cpp
+++ Shooter.cpp
@@ -1,42 +1,45 @@
 #include "WPILib.h"
 #include "Shooter.h"
-		
-		enum shooterStates{released, loaded};
 
 Shooter::Shooter() :
-    shooterMotor(PHOENIX2014_SHOOTER_LOAD_PWM),
-    retractedSensor(3),
-    releaseShooter(PHOENIX2014_SHOOTER_RELEASE),
-    shooterEncoder(5,6)
+   shooterMotor(PHOENIX2014_SHOOTER_LOAD_PWM),
+   retractedSensor(3),
+   releaseShooter(PHOENIX2014_SHOOTER_RELEASE),
+   shooterEncoder(5,6),
+   shooterState(loading)
 {
-    shooterEncoder.Reset();
+   shooterEncoder.Reset();
 }
 
 void Shooter::OperateShooter(Joystick * gamePad) {
 
-	
-	bool loadShooterButton = gamePad->GetRawButton(7);
-	
+
+	bool loadShooterButton = gamePad->GetRawButton(7);//TODO make constants
 	bool releaseShooterButton = gamePad->GetRawButton(8);
-	
 	bool isRetracted = retractedSensor.Get();
-	
 	int ShooterEncoderLimit = 100;
 	
-	if(releaseShooterButton && loaded){
-		releaseShooter.Set(Relay::kReverse);
-	}//Here I want start loading(retracting)
-	if (loadShooterButton &&  released && !isRetracted) {
-		shooterEncoder.Start();
-		shooterMotor.Set(1.0);
-	}//Read encoder if limit reached stop retracting
-	if(shooterEncoder.Get() >= ShooterEncoderLimit || isRetracted){
-		shooterMotor.Set(0.0);
+	//Here I want to shoot the ball
+	if(releaseShooterButton && shooterState == loaded){
+	releaseShooter.Set(Relay::kReverse);
+	shooterState = released;
 	}
-}
-
+	
+	//Here I want start loading(retracting)
+	if (loadShooterButton &&  shooterState == released && !isRetracted) {
+	shooterEncoder.Start();
+	shooterMotor.Set(1.0);
+	shooterState = loading;
+	}
+	
+	//Read encoder if limit reached stop retracting
+	if(shooterEncoder.Get() >= ShooterEncoderLimit || isRetracted){
+	shooterMotor.Set(0.0);
+	}
+	}
+	
 Shooter::~Shooter(){
-        
+		   
 }
 
 
